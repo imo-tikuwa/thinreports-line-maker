6231d3ea205285dfa39424007eba15d0737ca13e
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-01-19 00:06:40 +0900

    stop関数を修正
    killallでrubyと付くプロセスを雑に全部killしてたstop関数について、port番号を元にプロセスIDを取得してkillする形に修正

--- server/server.sh
+++ server/server.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+PORT=3001
 APP_ROOT=$(dirname $(cd $(dirname $0); pwd))
 cd $APP_ROOT
 
 start() {
-  bundle exec ruby app.rb -o 0.0.0.0 -p 3001 >> $APP_ROOT/log/application.log 2>&1 &
+  bundle exec ruby app.rb -o 0.0.0.0 -p $PORT >> $APP_ROOT/log/application.log 2>&1 &
 }
 stop() {
-  killall -9 ruby
+  lsof -i :$PORT | awk 'NR > 1 {print $2}' | xargs kill -9
 }
 
 case "$1" in
